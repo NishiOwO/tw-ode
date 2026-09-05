d5e3ade60fd49e18d4436bbce58617721882674c
Nishi <nishi@nishi.boats>
2026-09-05 18:41:11 +0900

    fix errors

--- ode.offline.js
+++ ode.offline.js
@@ -154,7 +154,6 @@
     dJointAddPUTorques;
   let ode;
   let embedded = false;
-  var ODEWASM;
   let worlds = {};
   let geoms = {};
   let bodies = {};
@@ -2601,7 +2600,7 @@ if (typeof exports === 'object' && typeof module === 'object') {
 embedded = true;
 
   if (embedded) {
-    ode = ODEWASM;
+    ode = ODEWASM; // eslint-disable-line
   } else {
     ode = await Scratch.external.evalAndReturn(
       "https://raw.githubusercontent.com/NishiOwO/tw-ode/e5f54b5e05ae363e937740af0c81b217e06c15e8/odejs.js",
